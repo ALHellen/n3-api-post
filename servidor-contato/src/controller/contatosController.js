b34224a65f6cc9e44ee463dfbeeee933fc6a9bd9
Telcrika <telcrika31@gmail.com>
2019-11-05 22:14:19 -0300

    Adiciona o post e importa bodyParser

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -5,14 +5,20 @@ const getAll = (request, response) => {
   response.status(200).send(contatos.model);
 };
 
-const getById = (request, response) => {
-  const id = request.params.id;
-  response.status(200).send(contatos.find(tarefa => tarefa.id == id));
-};
+// const getById = (request, response) => {
+//   const id = request.params.id;
+//   response.status(200).send(contatos.find(tarefa => tarefa.id == id));
+// };
+
+const add = (request, response, contato) => {
+  contatos.model.contatos.push(request.body)
+  console.log(request.body)
+  response.status(200).send(request.body)
+}
 
 
 module.exports = {
   getAll,
-  getById
+  add
 }
 
